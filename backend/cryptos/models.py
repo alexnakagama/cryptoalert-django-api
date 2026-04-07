8d52180628ff26b558c9aacc3270fdc2a86dec80
alex <alexnakagama4@gmail.com>
2026-04-07 19:36:01 -0300

    feat(cryptos-models): added cryptopricehistory model

--- backend/cryptos/models.py
+++ backend/cryptos/models.py
@@ -18,3 +18,13 @@ class Crypto(models.Model):
     
     def __str__(self):
         return f"{self.name} ({self.symbol})"
+    
+class CryptoPriceHistory(models.Model):
+    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE, related_name='prices')
+    price = models.DecimalField(max_length=20, decimal_places=8)
+    price_24h = models.DecimalField(max_digits=30, decimal_places=2, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-timestamp']
+        unique_together = ('crypto', 'timestamp')
