8c1d65cf87482650c464df66729930a4de2dbbf1
alex <alexnakagama4@gmail.com>
2026-04-08 02:47:06 -0300

    feat(services): created functions to get the price of a cripto, min and max

--- backend/cryptos/services.py
+++ backend/cryptos/services.py
@@ -11,3 +11,27 @@ def get_price(url: str, params=None):
         
 def get_crypto_price(symbol: str, currency: str, volume):
     return get_price(f'https://criptoya.com/api/{symbol}/{currency}/{volume}')
+
+def get_min_price(symbol, currency, volume):
+    data = get_crypto_price(symbol, currency, volume)
+    if not data:
+        return None
+    prices = []
+    for exchange in data:
+        if 'totalAsk' in data[exchange]:
+            prices.append(data[exchange]['totalAsk'])         
+    if prices:
+        return min(prices)
+    return None
+
+def get_max_price(symbol, currency, volume):
+    data = get_crypto_price(symbol, currency, volume)
+    if not data:
+        return None
+    prices = []
+    for exchange in data:
+        if 'totalAsk' in data[exchange]:
+            prices.append(data[exchange]['totalAsk'])         
+    if prices:
+        return max(prices)
+    return None
